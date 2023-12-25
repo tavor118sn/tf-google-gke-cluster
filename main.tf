ad3e56997b14f98468195630efbdbe38f63cdc68
Andrew Petriv <andrey.petriv@starnavi.io>
2023-12-25 18:28:23 +0200

    Add disk_size_gb for machine

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ resource "google_container_cluster" "this" {
   initial_node_count       = 1
   remove_default_node_pool = true
 
-    workload_identity_config {
+  workload_identity_config {
     workload_pool = "${var.GOOGLE_PROJECT}.svc.id.goog"
   }
   node_config {
-        workload_metadata_config {
+    workload_metadata_config {
       mode = "GKE_METADATA"
     }
   }
@@ -30,6 +30,7 @@ resource "google_container_node_pool" "this" {
 
   node_config {
     machine_type = var.GKE_MACHINE_TYPE
+    disk_size_gb = 400
   }
 }
 
@@ -37,16 +38,16 @@ module "gke_auth" {
   depends_on = [
     google_container_cluster.this
   ]
-  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  version              = ">= 24.0.0"
-  project_id           = var.GOOGLE_PROJECT
-  cluster_name         = google_container_cluster.this.name
-  location             = var.GOOGLE_REGION
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version      = ">= 24.0.0"
+  project_id   = var.GOOGLE_PROJECT
+  cluster_name = google_container_cluster.this.name
+  location     = var.GOOGLE_REGION
 }
 
 resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "${path.module}/kubeconfig"
+  content         = module.gke_auth.kubeconfig_raw
+  filename        = "${path.module}/kubeconfig"
   file_permission = "0400"
 }
 
